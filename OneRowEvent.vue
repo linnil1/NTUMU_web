e85c8f27a6c4f8c38a2fe63c170a8e1b70e363b8
linnil1 <linnil1.886@gmail.com>
2017-02-20 02:15:00 +0800

    one row event

---
+++ OneRowEvent.vue
@@ -0,0 +1,98 @@
+<template>
+	<div class="background-events" :style="[height,{width:(range*width)+'px'}]">
+		<div class="background-all" :style="height">
+			<div class="background-box" :style="{width:width+'px'}" v-for="i in range"></div>
+		</div>
+		<div class="event" v-for="evt in eventSort" :style="styleGet(evt)">
+			{{evt.title}}</div>
+	</div>
+</template>
+
+<script>
+export default {
+	name: 'onerowevent',
+	props: {
+		width: {
+			type: Number,
+			default: 60
+		},
+		range: {
+			type: Number,
+			default: 16 
+		},
+		eventdata: {
+			type: Array,
+			default: []
+		}
+	},
+	data () { return {
+		height: {'height':'1.5em'}
+	}},
+	methods: {
+		styleGet: function(evt){
+			return {
+				left: evt.start * this.width + 'px',
+				width: (evt.end - evt.start) * this.width + 'px',
+				top: evt.rank + 'em',
+				color: evt.color,
+				'background-color': evt.bgcolor 
+			}
+		},
+	},
+	computed: {
+		eventSort: function(){
+			if(!this.eventdata)
+				return []
+			// sorted from first to last
+			var sortedevent = this.eventdata.sort( function(a,b){
+				if(a.start != b.start)
+					return a.start > b.start
+				else
+					return a.end > b.start
+			})
+			// set sorted rank
+			var step = 0;
+			sortedevent[0].rank = step
+			for(var evt in sortedevent)
+				if(evt != 0 && sortedevent[evt-1].end > sortedevent[evt].start)
+					sortedevent[evt].rank = step += 1.5
+				else
+					sortedevent[evt].rank = step 
+			this.height.height = step+1.5+'em'
+			console.log(sortedevent)
+			return sortedevent
+		}
+	}
+}
+</script>
+
+<style scoped>
+.background-box{
+	border-right: 1px solid #ddd;
+	float: left;
+	width: 60px;
+	height: 100%;
+}
+.background-box:nth-child(odd){
+	border-right: 1px dotted #ddd;
+}
+.background-all{
+	border: 1px solid #ddd;
+	position: relative;
+	z-index: -5;
+	overflow: hidden;
+	height: 3em;
+}
+.background-events{
+	position: relative;
+	width: 100%;
+}
+.event{
+	position: absolute;
+	background-color: #9f3;
+	height: 1.5em;
+	text-align: center;
+	font-size: 1em;
+	overflow: hidden;
+}
+</style>
